a295423a190141b9166deeb38b1dfc627d3a8861
Ｔｙｃｏｏｎ <90176757+sanketkumkar@users.noreply.github.com>
2023-04-08 13:52:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ API_HASH = os.environ.get("API_HASH", "32b77438d290b88d51c051f029c36fb5")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5868634376:AAGpCDLTK09d8mIy_vhW3BRamLAIHwZYJEQ")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("1897413160")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "mongosh")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:k9PKUPJKGPKSihEq202e@containers-us-west-168.railway.app:6440/railway")"
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "adanylinks")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://mongo:9cyAqeVb36ChW9BSHzeK@containers-us-west-7.railway.app:6035")
 OWNER_ID =  int(os.environ.get("OWNER_ID", "1897413160")) 
 ADMINS.append(1897413160) if OWNER_ID not in ADMINS else []
 ADMINS.append(1897413160)
